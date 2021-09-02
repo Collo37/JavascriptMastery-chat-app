338d5bd3957011ff015fae5e90fecf9c364c0062
collins <cowlduor37@gmail.com>
2021-09-02 05:53:18 +0300

    added 3 new components
    added messageForm, My message and theirMessage components

---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,12 @@
+import MessageForm from "./MessageForm";
+import MyMessage from "./MyMessage";
+import TheirMessage from "./TheirMessage";
+
+const ChatFeed = (props) => {
+  const { chats, activeChat, userName, messages } = props;
+  const chat = chats && chats[activeChat];
+  console.log(chat, userName, messages);
+  return <div>Chat feed</div>;
+};
+
+export default ChatFeed;
---
+++ src/components/MyMessage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyMessage = () => {
+  return <div>Message</div>;
+};
+
+export default MyMessage;
